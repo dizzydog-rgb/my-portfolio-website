bab39a18553350c580a8729ff77d16a5f40e2513
Jackie_Chen <fox29582@yahoo.com.tw>
2026-03-21 14:44:07 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, process } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/my-portfolio-website/' : '/',
-  
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
